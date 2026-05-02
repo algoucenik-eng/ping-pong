29f3bdb1aad3bc1c9f484731d7a57ca1cbe25bf0
algoucenik-eng <algoucenik@gmail.com>
2026-05-02 14:33:25 +0300

    Update ping_pong.py
    Создание класса-наследника для спрайта-игрока.

--- ping_pong.py
+++ ping_pong.py
@@ -19,3 +19,18 @@ class GameSprite(sprite.Sprite):
 
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+
+#класс-наследник для спрайта-игрока (управляется стрелками)
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
